a2b7e6e92db4c1b56d2b3f838c8ab2f257b62e80
MissCinthya <91693293+MissCinthya@users.noreply.github.com>
2021-11-09 17:46:40 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,24 @@
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+
+var engine,world;
+var cuerpo;
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  engine=Engine.create();
+  world=engine.world;
+  cuerpo=Bodies.rectangle(200,100,50,50);
+ World.add(world,cuerpo); 
+ console.log(cuerpo.position);
+ console.log(cuerpo.position.x);
+ console.log(cuerpo.position.y);
 }
 
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+  background(0);  
+rectMode(CENTER);
+  rect(cuerpo.position.x,cuerpo.position.y, 50, 50);
+Engine.update(engine);
+ 
 }
